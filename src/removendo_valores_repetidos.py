6e0ad3ddc70f462c42bebeba1baab2a0cd734e88
Renato Veneroso <rveneroso@gmail.com>
2022-02-10 10:24:37 -0300

    Ajuste no índice e no nome da coluna de índice no DataFrame filtrado.

--- src/removendo_valores_repetidos.py
+++ src/removendo_valores_repetidos.py
@@ -8,4 +8,12 @@ tipos_de_imoveis = dados['Tipo'] # Retorna uma variável do tipo <class 'pandas.
 # tem valor False. Se esse parâmetro não for setado para True, o método drop_duplicates() não surtirá efeito
 # na variável sendo alterada. Nesse caso é preciso atribuir o resultado de drop_duplicates() à alguma variável.
 tipos_de_imoveis.drop_duplicates(inplace=True)
+
+# Como mencionado acima, tipos_de_imoveis é uma Series. Para transformá-la em um DataFrame:
+tipos_de_imoveis = pd.DataFrame(tipos_de_imoveis) # <class 'pandas.core.frame.DataFrame'>
+
+# O DataFrame acima terá os índices correspondentes às linhas utilizadas por ele na remoção de
+# duplicidades. Para refazer esses índices tornando-os sequenciais:
+tipos_de_imoveis.index = range(tipos_de_imoveis.shape[0])
+tipos_de_imoveis.columns.name = 'Id'
 print(tipos_de_imoveis)
