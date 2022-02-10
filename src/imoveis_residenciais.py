256bec46bae2b034028a656758576a9f4850e596
Renato Veneroso <rveneroso@gmail.com>
2022-02-10 12:34:28 -0300

    Criação do script que extrai do arquivo csv somente os imóveis que são residenciais.

---
+++ src/imoveis_residenciais.py
@@ -0,0 +1,17 @@
+import pandas as pd
+dados = pd.read_csv('../data/aluguel.csv', sep = ';')
+
+# Cria a lista de tipos relacionados à imóveis residenciais
+residencial = ['Quitinete',
+'Casa',
+'Apartamento',
+'Casa de Condomínio',
+'Casa de Vila']
+
+# Verifica, dentre os imóveis presentes no DataFrame, quais são do tipo que pode ser considerado residencial.
+# Todos os imóveis estarão presentes no resultodo mas os que não forem residenciais estarão com o valor False.
+selecao = dados['Tipo'].isin(residencial)
+dados_residencial = dados[selecao]
+# Reindexa o DataFrame
+dados_residencial.index = range(dados_residencial.shape[0])
+print(dados_residencial)
